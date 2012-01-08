9b01d5a7fd574e99577e91913a5e540925e1d4a0
Rok Garbas <rok@garbas.si>
2012-01-08 01:23:24 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+Introduction
+============
+
+plone.app.jquery adds `jquery`_ library to Plone.
+
+.. `jquery`: http://jquery.com
---
+++ plone/app/jquery/upgrades.py
@@ -0,0 +1,19 @@
+import logger
+from Products.CMFCore.utils import getToolByName
+
+
+class ImportProfileStep(object):
+    """import of GenericSetup profile"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, context):
+        logger.info("Start importing profile: " + self.name)
+        tool = getToolByName(context, 'portal_setup')
+        tool.runAllImportStepsFromProfile(self.name)
+        logger.info("Stop importing profile: " + self.name)
+
+
+step_1_initial_upgrade = ImportProfileStep(
+    u'profile-plone.app.jquery.:initial-upgrade')
