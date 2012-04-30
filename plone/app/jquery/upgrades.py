331ba244fb8204ce656e5272b09dde33639a4912
Johannes Raggam <raggam-nl@adm.at>
2012-04-30 16:54:58 +0200

    add upgrade step to call cookResource

--- plone/app/jquery/upgrades.py
+++ plone/app/jquery/upgrades.py
@@ -16,5 +16,15 @@ class ImportProfileStep(object):
         logger.info("Stop importing profile: " + self.name)
 
 
+def common_upgrade(context):
+    """ Common upgrade for newer jQuery versions, just to call cookResources.
+    """
+    tool = getToolByName(context, 'portal_javascripts')
+    tool.cookResources()
+    logger.info("Cooked the JS Resouces")
+
+
 step_1_initial_upgrade = ImportProfileStep(
     u'profile-plone.app.jquery:initial-upgrade')
+
+upgrade_2_to_3 = common_upgrade
