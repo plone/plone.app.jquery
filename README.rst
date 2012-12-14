4180b8a7e6e871dd71222f402c3c4392b196934d
Rok Garbas <rok@garbas.si>
2012-12-14 17:15:44 +0100

    updating to jquery 1.7.2

--- README.rst
+++ README.rst
@@ -8,12 +8,9 @@ Versions
 
 Version numbers of the package is the same as the jQuery library + packaging version.
 
-Available versions of this package are
-
-::
-
-  plone.app.jquery = 1.7.1.1
-  plone.app.jquery = 1.4.4
+Available versions of this package are:
+- 1.7.2
+- 1.4.4
 
 jQuery 1.7 is not supported on Plone < 4.3. So if you have dependencies on plone.app.jquery
 you should pin it in your buildout configuration to 1.4.4.
--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 from setuptools import setup, find_packages
 
-version = '1.7.1.2.dev0'
+version = '1.7.2'
 
-setup(name='plone.app.jquery',
-      version=version,
-      description="jQuery integration for Plone",
-      long_description=open("README.rst").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-        ],
-      keywords='Plone jQuery',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.app.jquery',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.CMFCore',
-          'Products.GenericSetup',
-      ],
-      entry_points="""
-          [z3c.autoinclude.plugin]
-          target = plone
-      """,
-      extras_require={'test': ['plone.app.testing']},
+setup(
+    name='plone.app.jquery',
+    version=version,
+    description="jQuery integration for Plone",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='Plone jQuery',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.app.jquery',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFCore',
+        'Products.GenericSetup',
+    ],
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+    """,
+    extras_require={'test': ['plone.app.testing']},
 )
