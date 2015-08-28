53d40b641922bad5eec70036be3ae126f52eafc5
Maurits van Rees <maurits@vanrees.org>
2015-08-28 16:31:36 +0200

    Claim a few classifiers in setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
     ],
     keywords='Plone jQuery',
     author='Plone Foundation',
